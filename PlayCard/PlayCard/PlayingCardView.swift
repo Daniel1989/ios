8c3403a489e0fe84a429abed428f1c0d8f9ba1b5
三山 <sanshan@ywwl.com>
2024-11-28 13:46:08 +0800

    feat: update playing card

---
+++ PlayCard/PlayCard/PlayingCardView.swift
@@ -0,0 +1,53 @@
+//
+//  PlayingCardView.swift
+//  PlayCard
+//
+//  Created by 曹肖鹏 on 2024/11/28.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+    var rank: Int
+    var suit: String
+
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle, .font: font])
+        
+    }
+    
+    private var cornetString: NSAttributedString {
+        return centeredAttributedString(rank + "\n" + suit, fontSize: 0.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+//        if let context = UIGraphicsGetCurrentContext() {
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            context.strokePath()
+//            context.fillPath()
+//        }
+        
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.lineWidth = 5.0
+//        UIColor.green.setFill()
+//        UIColor.red.setStroke()
+//        path.stroke()
+//        path.fill()
+        
+        
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+    }
+    
+
+}
