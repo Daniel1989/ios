5ecba1dde4edf9a7d6460dd6ef2ef1669491a15a
三山 <sanshan@ywwl.com>
2024-12-16 13:32:42 +0800

    feat: update

---
+++ Concentration/ConcentrationThemeChooseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConcentrationThemeChooseViewController.swift
+//  Concentration
+//
+//  Created by 曹肖鹏 on 2024/12/10.
+//
+
+import UIKit
+
+class ConcentrationThemeChooseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class COncentrationViewController: UIViewController {
     
     lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
