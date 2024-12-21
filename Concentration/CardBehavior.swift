e0c99ce8111b5a8bea34e898a377af3224b2f061
三山 <sanshan@ywwl.com>
2024-12-21 13:44:25 +0800

    feat: update aniamtor

---
+++ Concentration/CardBehavior.swift
@@ -0,0 +1,57 @@
+//
+//  CardBehavior.swift
+//  Concentration
+//
+//  Created by 曹肖鹏 on 2024/12/19.
+//
+
+import UIKit
+
+class CardBehavior: UIDynamicBehavior {
+    lazy var collisionBehavior: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.elasticity = 1.0
+        behavior.resistance = 0
+        return behavior
+    }()
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        push.angle = CGFloat((Int(2*CGFloat.pi)).arc4random)
+        push.magnitude = CGFloat(1.0) + CGFloat((Int(2)).arc4random)
+        push.action = { [unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    func addItem(_ item: UIDynamicItem) {
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    func removeItem(_ item: UIDynamicItem) {
+        collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
+        
+    }
+    
+    override init() {
+        super.init()
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        animator.addBehavior(self)
+    }
+}
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+class CustomTapGesture: UITapGestureRecognizer {
+    var customParameter: Int?
+}
+
 class ConcentrationViewController: UIViewController {
     
     lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    
+    
     
     var numberOfPairsOfCards: Int {
         return (cardButtons.count + 1) / 2
@@ -84,14 +92,133 @@ class ConcentrationViewController: UIViewController {
                     button.setTitle("", for: UIControl.State.normal)
                     button.backgroundColor = card.isMatched ? UIColor.clear : UIColor.blue
                 }
+                // 第一个点击走的是touch事件
+                // 在这里由于重新覆盖之前的touch，所以不走tap，走了这里
+                // 使用CustomTap来传递额外参数
+                let tapGesture = CustomTapGesture(target: self, action: #selector(filpCardWithTap(_:)))
+                
+                button.addGestureRecognizer(tapGesture)
+                tapGesture.customParameter = index
+                
             }
         }
         
     }
     
+    private var faceUpCardView: [UIButton] {
+        return cardButtons.filter {
+            !$0.isHidden && $0.backgroundColor != UIColor.blue
+        }
+    }
+    
+    private var faceUPCardViewMatch: Bool {
+        return faceUpCardView.count == 2 && faceUpCardView[0].currentTitle == faceUpCardView[1].currentTitle
+    }
+    
+    @objc func filpCardWithTap(_ recognizer: CustomTapGesture) {
+        print("hahah")
+        let index = recognizer.customParameter!
+        
+        switch recognizer.state {
+        case .ended:
+            game.chooseCard(at: index)
+            let card = game.cards[index]
+            if card.isFaceUp {
+                
+                if let chosenCardView = recognizer.view as? UIButton {
+                    chosenCardView.setTitle(emoji(for: card), for: UIControl.State.normal)
+                    chosenCardView.backgroundColor = UIColor.gray
+                    chosenCardView.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+                    cardBehavir.removeItem(chosenCardView);                    UIView.transition(with: chosenCardView,
+                                      duration: 0.6,
+                                      options: [.transitionFlipFromLeft],
+                                      animations: {
+                        
+                    },
+                                      completion: {
+                        finished in
+                        if self.faceUPCardViewMatch {
+                            UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.6, delay: 0, animations: {
+                                self.faceUpCardView.forEach {
+                                    $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                                }
+                            },
+                                                                           completion: { position in
+                                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.75, delay: 0, animations: {
+                                    self.faceUpCardView.forEach {
+                                        $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                        $0.alpha = 0
+                                    }
+                                },
+                                                                               completion: {
+                                    position in
+                                    self.faceUpCardView.forEach {
+                                        $0.isHidden = true
+                                        $0.alpha = 1
+                                        $0.transform = .identity
+                                    }
+                                }
+                                )
+                                
+                                
+                            }
+                            )
+                        }
+                        else if self.faceUpCardView.count == 2 {
+                            self.faceUpCardView.forEach { tempView in
+                                UIView.transition(with: tempView, duration: 0.6,
+                                                  options: [.transitionFlipFromLeft],
+                                                  animations: {
+                                    tempView.backgroundColor = UIColor.blue
+                                    tempView.setTitle("", for: UIControl.State.normal)
+                                    
+                                },
+                                                  completion:  {
+                                    finished in
+                                    self.cardBehavir.addItem(tempView)
+                                }
+                                )
+                            }
+                        } else {
+                            if !chosenCardView.isHidden {
+                                self.cardBehavir.addItem(chosenCardView)
+                            }
+                        }
+                    }
+                    )
+                    
+                }
+                
+            } else {
+                if let chosenCardView = recognizer.view as? UIButton {
+                    chosenCardView.setTitle("", for: UIControl.State.normal)
+                    chosenCardView.backgroundColor = card.isMatched ? UIColor.clear : UIColor.blue
+                }
+            }
+        default:
+            break
+            
+        }
+    }
+    
+    lazy var cardBehavir = CardBehavior(in: animator)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for index in cardButtons.indices {
+            let tapGesture = CustomTapGesture(target: self, action: #selector(filpCardWithTap(_:)))
+            tapGesture.customParameter = index
+            let button = cardButtons[index]
+            button.addGestureRecognizer(tapGesture)
+            cardBehavir.addItem(button)
+//            let push = UIPushBehavior(items: [button], mode: .instantaneous)
+//            push.angle = CGFloat((Int(2*CGFloat.pi)).arc4random)
+//            push.magnitude = CGFloat(1.0) + CGFloat((Int(2)).arc4random)
+//            push.action = { [unowned push] in
+//                push.dynamicAnimator?.removeBehavior(push)
+//            }
+//            animator.addBehavior(push)
+        }
         // Do any additional setup after loading the view.
     }
     
